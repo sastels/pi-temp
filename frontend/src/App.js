4393f9beb0d0c871fe43123a5d88e4981bd8d42f
Steve Astels <sastels@gmail.com>
2019-03-10 21:45:56 -0400

    maybe get react firebase stuff working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 
+import { signIn, loadData } from "../utils/firebase";
+
+
 class App extends Component {
+
+  state = {
+    data: [],
+};
+
+  componentDidMount() {
+    signIn(() => loadData(this.props.id, this.setState.bind(this)));
+}
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1> Temperatures</h1>
+
+
+
       </div>
     );
   }
