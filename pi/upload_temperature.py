54cade5880216f0f63d6a28fe99aa8e1c079f027
Steve Astels <sastels@users.noreply.github.com>
2019-03-12 19:18:48 -0400

    switch to python 3.5 (#3)

--- pi/upload_temperature.py
+++ pi/upload_temperature.py
@@ -2,16 +2,18 @@ import os
 import time
 from dotenv import load_dotenv
 from firebase_utils import setup_firebase, upload_to_firebase
-from sensor_utils import setup_sensor, read_temp
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT22
+pin = 4
 
 if __name__ == "__main__":
     load_dotenv()
-
     pi_id = os.getenv("PI_ID")
+    measurement_interval = os.getenv("INTERVAL", default=60*10)
     db = setup_firebase('./service_account.json')
-    device_file = setup_sensor()
 
     while True:
-        temperature = read_temp(device_file)
-        upload_to_firebase(db=db, pi_id=pi_id, temperature=temperature)
-        time.sleep(10)
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        upload_to_firebase(db=db, pi_id=pi_id, temperature=temperature, humidity=humidity)
+        time.sleep(measurement_interval)
