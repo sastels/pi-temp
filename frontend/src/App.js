e240bc63885330fc0fa58045c1f2328f031bd868
Steve Astels <sastels@users.noreply.github.com>
2019-03-15 14:04:12 -0400

    add snapshoting and show current conditions (#9)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,9 +16,15 @@ class App extends Component {
   }
 
   render() {
+    let currentConditions = "(no data)"
+    if (this.state.data.length > 0) {
+      const m = this.state.data.slice(-1)[0]
+      currentConditions = `${m.datetime.fromNow()}: ${m.temperature.toFixed(1)}°C ${m.humidity.toFixed(1)} %`
+    }
     return (
       <div>
         <h1>{this.state.id}</h1>
+        <h2> {currentConditions}</h2>
         <Graph data={this.state.data}/>
       </div>
     );
